cbeeaecd7df83f9c9274718efa76b9dbae81805a
Kotha Hemanth <hemanthkotha17@gmail.com>
2024-03-13 16:48:55 +0530

    Step-10

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -215,8 +215,23 @@ sap.ui.define([
 		 _setDetailArea : function (oUserContext) {
             var oDetailArea = this.byId("detailArea"),
                 oLayout = this.byId("defaultLayout"),
+                oOldContext,
                 oSearchField = this.byId("searchField");
  
+            if (!oDetailArea) {
+                return; // do nothing when running within view destruction
+            }
+ 
+            oOldContext = oDetailArea.getBindingContext();
+            if (oOldContext) {
+                oOldContext.setKeepAlive(false);
+            }
+            if (oUserContext) {
+                oUserContext.setKeepAlive(true,
+                    // hide details if kept entity was refreshed but does not exists any more
+                    this._setDetailArea.bind(this));
+
+            }
             oDetailArea.setBindingContext(oUserContext || null);
             // resize view
             oDetailArea.setVisible(!!oUserContext);
